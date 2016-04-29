b31724b1f6afe0968b0d2b978719792804e06346
Brian Blaylock <blaylockbk@users.noreply.github.com>
2016-04-29 11:29:40 -0600

    Update redefine_matplotlib_settings.py

--- redefine_matplotlib_settings.py
+++ redefine_matplotlib_settings.py
@@ -9,33 +9,36 @@ publication quality figures.
 import matplotlib as mpl
 
 # Specify universal figure sizes acceptable for publication
-# J.AtmosEnv
-#     Figure Page Width   |   Inches Wide       
-#   ----------------------+---------------------
-#   1/2 page (1 column)   |  3.54 in. ( 90 mm)   
-#   3/4 page (1.5 column) |  5.51 in. (140 mm)    
-#   1   page (2 column)   |  7.48 in. (190 mm)    
-#   ---------------------------------------------
-# Image Resolution:  line graphs : 1000 dpi
-#                    colorfilled : 500 dpi         
-#                    web images  : 72 dpi             
+    # J.AtmosEnv
+    #        Figure width     |   Inches wide       
+    #   ----------------------+---------------------
+    #     minimumal size      |  1.18 in. ( 30 mm)
+    #   1/2 page (1 column)   |  3.54 in. ( 90 mm)   
+    #   3/4 page (1.5 column) |  5.51 in. (140 mm)    
+    #   1   page (2 column)   |  7.48 in. (190 mm)    
+    #   ---------------------------------------------
+    # Image Resolution:  line drawing = 1000 dpi
+    #                    colorfilled  = 500 dpi         
+    #                    photographs  = 300 dpi                                 
+    #                    for web      = 72 dpi
 
-label_font  = 10    
-tick_font   = 8 
-legend_font = 7
-
-width=3.5
-height=3
-
-## Reset the defaults
-mpl.rcParams['axes.labelsize']  = label_font
-mpl.rcParams['xtick.labelsize'] = tick_font
-mpl.rcParams['ytick.labelsize'] = tick_font
-mpl.rcParams['legend.fontsize'] = legend_font
-
-mpl.rcParams['figure.figsize'] = [width,height] 
-
-mpl.rcParams['grid.linewidth'] = .25
-
-mpl.rcParams['savefig.bbox'] = 'tight'
-mpl.rcParams['savefig.dpi'] = 1000
+    label_font  = 10    
+    tick_font   = 8 
+    legend_font = 7
+    
+    width=3.5  # refer to above table
+    height=3   # adjust as needed, but bbox="tight" should take care of most of this
+    
+    
+    ## Reset the defaults (see more here: http://matplotlib.org/users/customizing.html)
+    mpl.rcParams['xtick.labelsize'] = tick_font
+    mpl.rcParams['ytick.labelsize'] = tick_font
+    mpl.rcParams['axes.labelsize'] = label_font
+    mpl.rcParams['legend.fontsize'] = legend_font
+    
+    mpl.rcParams['figure.figsize'] = [width,height] 
+    
+    mpl.rcParams['grid.linewidth'] = .25
+    
+    mpl.rcParams['savefig.bbox'] = 'tight'
+    mpl.rcParams['savefig.dpi'] = 1000
